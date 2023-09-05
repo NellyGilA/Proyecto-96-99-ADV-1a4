dafad90d13623890912dc245b1203bc814bf1e69
NellyGilA <86023328+NellyGilA@users.noreply.github.com>
2023-09-05 17:35:43 -0600

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -1,9 +1,12 @@
-function addUser() {
-    player1_name = document.getElementById("player1_name_input").value;
-    player2_name = document.getElementById("player2_name_input").value;
-  
-      //Establece "player1_name" usando la función localStorage.setItem()
-        //Establece "player2_name" usando la función localStorage.setItem()
-  
-      window.location = "game_page.html";
+// Crea la función addUser()
+function () {
+  // Obtén el valor del usuario con id player1_name_input y player2_name_input
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+  // Almacena los valores en el almacenamiento local
+  //Establece "player1_name" usando la función localStorage.setItem()
+	  
+
+   //Asigna "game_page.html" a window.location
 }
